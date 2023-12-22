638e1f5fc7359a50ce8b9ad96b1011fd37301516
Shambhavi Ray <95281690+Shambhairay@users.noreply.github.com>
2023-12-22 20:17:26 +0000

    datatype

---
+++ .devcontainer/001_basics/02_variables.js
@@ -0,0 +1,17 @@
+let age=18
+let isLoggedIn=true
+let studentName="Mantosh"
+let state=null
+
+//number=2 to power 53
+//bigInt
+//string=" "
+//boolean=true/false
+//null=standalone value
+//undefined=when we dont define any value: let address;
+//symbol=unique
+console.log(typeof age)
+
+//object=null is object ,after running type of null it will return object,some people say that it is error in javascript
+console.log(typeof null)
+console.log(typeof undefined)//undefined

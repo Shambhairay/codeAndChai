ca3e5aedbf7c7585d438dbc68979d9fcd24a8809
Shambhavi Ray <95281690+Shambhairay@users.noreply.github.com>
2023-12-22 20:20:31 +0000

    variable

---
+++ .devcontainer/001_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountPassword=1234
+let accountEmailAddress="sham123@gmail.com"
+var accountCity= "banglore"
+pincode=841211
+//password=2356  we cannot change the password because here we declare variable as a const
+accountEmailAddress="harsh@gmail.com"
+/*   prefer not to use var
+because of issue in block scope and functional scope
+scope is noting but :-{
+
+}
+*/
+accountCity="Kerla"
+console.log(accountPassword)
+console.table([accountPassword,accountEmailAddress,accountCity])
